d34641ceb65376d627c0b0abeacdc8bc9b6939ad
Adán Mauri Ungaro <adan.mauri@gmail.com>
2017-10-23 22:45:10 -0300

    + Union, replaceAll and Licence

---
+++ JsStringEnhancer.js
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2017 Adán Mauri Ungaro <adan.mauri@gmail.com>
+ *                Pablo Marcelo Costanzo <costanzo_pablo@hotmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+String.prototype.union = function(anotherStr) {
+	tmpStr = this;
+	tmpAnotherStr = anotherStr;
+
+	if (tmpStr.length < tmpAnotherStr.length) {
+		tmpAnotherStr = tmpAnotherStr.substring(0,tmpStr.length);
+	} else if (tmpAnotherStr.length < tmpStr.length) {
+		tmpStr = tmpStr.substring(tmpStr.length-tmpAnotherStr.length, tmpStr.length);
+	}
+
+	do {
+		if (tmpStr.includes(tmpAnotherStr)) {
+			break;
+		}
+		tmpStr = tmpStr.substring(1,tmpStr.length);
+		tmpAnotherStr = tmpAnotherStr.substring(0,tmpAnotherStr.length-1);
+	} while (tmpStr.length > 0 & tmpAnotherStr.length > 0);
+	str = this + anotherStr.substring(tmpAnotherStr.length,anotherStr.length);
+	return str;
+}
+
+String.prototype.replaceAll = function(origin, replacement) {
+	str = this;
+	return str.split(origin).join(replacement);
+}
